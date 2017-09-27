1e247eea26d0142c827b067394b7a0025a47be76
Morgan Bazalgette <the@howl.moe>
2017-09-27 10:23:00 +0200

    Add expvar request handler

--- api/index.go
+++ api/index.go
@@ -1,10 +1,21 @@
 package api
 
+import (
+	"expvar"
+)
+
 func index(c *Context) {
 	c.WriteHeader("Content-Type", "text/plain; charset=utf-8")
 	c.Write([]byte("CheeseGull v2.x Woo\nFor more information: https://github.com/osuripple/cheesegull"))
 }
 
+var _evh = expvar.Handler()
+
+func expvarHandler(c *Context) {
+	_evh.ServeHTTP(c.writer, c.Request)
+}
+
 func init() {
 	GET("/", index)
+	GET("/expvar", expvarHandler)
 }
