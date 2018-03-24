a9679fc911847046f75c12c477e5ce47e9d96749
Morgan Bazalgette <the@howl.moe>
2018-03-24 11:25:05 +0100

    Query mysql straight when no text query is given

--- models/set_search.go
+++ models/set_search.go
@@ -57,58 +57,83 @@ func sIntCommaSeparated(nums []int) string {
 // SearchSets retrieves sets, filtering them using SearchOptions.
 func SearchSets(db, searchDB *sql.DB, opts SearchOptions) ([]Set, error) {
 	sm := strconv.Itoa(int(opts.setModes()))
-	setIDsQuery := "SELECT id, set_modes & " + sm + " AS valid_set_modes FROM cg WHERE "
 
-	// add filters to query
-	// Yes. I know. Prepared statements. But Sphinx doesn't like them, so
-	// bummer.
-	setIDsQuery += "MATCH('" + mysqlStringReplacer.Replace(opts.Query) + "') "
+	// first, we create the where conditions that are valid for both querying mysql
+	// straight or querying sphinx first.
+	var whereConds string
+	var havingConds string
 	if len(opts.Status) != 0 {
-		setIDsQuery += "AND ranked_status IN (" + sIntCommaSeparated(opts.Status) + ") "
+		whereConds = "ranked_status IN (" + sIntCommaSeparated(opts.Status) + ") "
 	}
 	if len(opts.Mode) != 0 {
 		// This is a hack. Apparently, Sphinx does not support AND bitwise
 		// operations in the WHERE clause, so we're placing that in the SELECT
 		// clause and only making sure it's correct in this place.
-		setIDsQuery += "AND valid_set_modes = " + sm + " "
+		havingConds = " valid_set_modes = " + sm + " "
 	}
 
-	// set limit
-	setIDsQuery += fmt.Sprintf("ORDER BY WEIGHT() DESC, id DESC LIMIT %d, %d OPTION ranker=sph04, max_matches=10000000", opts.Offset, opts.Amount)
-
-	// fetch rows
-	rows, err := searchDB.Query(setIDsQuery)
-	if err != nil {
-		return nil, err
-	}
-
-	// from the rows we will retrieve the IDs of all our sets.
-	// we also pre-create the slices containing the sets we will fill later on
-	// when we fetch the actual data.
-	setIDs := make([]int, 0, opts.Amount)
 	sets := make([]Set, 0, opts.Amount)
-	// setMap, having an ID, points to a position of a set contained in sets.
+	setIDs := make([]int, 0, opts.Amount)
+	// setMap is used when a query is given to make sure the results are kept in the correct
+	// order given by sphinx.
 	setMap := make(map[int]int, opts.Amount)
-	for rows.Next() {
-		var id int
-		err = rows.Scan(&id, new(int))
+	// if Sphinx is used, limit will be cleared so that it's not used for the mysql query
+	limit := fmt.Sprintf(" LIMIT %d, %d ", opts.Offset, opts.Amount)
+
+	if opts.Query != "" {
+		setIDsQuery := "SELECT id, set_modes & " + sm + " AS valid_set_modes FROM cg WHERE "
+
+		// add filters to query
+		// Yes. I know. Prepared statements. But Sphinx doesn't like them, so
+		// bummer.
+		setIDsQuery += "MATCH('" + mysqlStringReplacer.Replace(opts.Query) + "') "
+		if whereConds != "" {
+			setIDsQuery += "AND " + whereConds
+		}
+		if havingConds != "" {
+			setIDsQuery += " AND " + havingConds
+		}
+
+		// set limit
+		setIDsQuery += " ORDER BY WEIGHT() DESC, id DESC " + limit + " OPTION ranker=sph04, max_matches=20000 "
+		limit = ""
+
+		// fetch rows
+		rows, err := searchDB.Query(setIDsQuery)
 		if err != nil {
 			return nil, err
 		}
-		setIDs = append(setIDs, id)
-		sets = append(sets, Set{})
-		setMap[id] = len(sets) - 1
+
+		// contains IDs of the sets we will retrieve
+		for rows.Next() {
+			var id int
+			err = rows.Scan(&id, new(int))
+			if err != nil {
+				return nil, err
+			}
+			setIDs = append(setIDs, id)
+			sets = sets[:len(sets)+1]
+			setMap[id] = len(sets) - 1
+		}
+
+		// short path: there are no sets
+		if len(sets) == 0 {
+			return []Set{}, nil
+		}
+
+		whereConds = "id IN (" + sIntCommaSeparated(setIDs) + ")"
+		havingConds = ""
 	}
 
-	// short circuit: there are no sets
-	if len(sets) == 0 {
-		return []Set{}, nil
+	if whereConds != "" {
+		whereConds = "WHERE " + whereConds
 	}
-
-	setsQuery := "SELECT " + setFields + " FROM sets WHERE id IN (" + inClause(len(setIDs)) + ")"
-	args := sIntToSInterface(setIDs)
-
-	rows, err = db.Query(setsQuery, args...)
+	if havingConds != "" {
+		havingConds = " HAVING " + havingConds
+	}
+	setsQuery := "SELECT " + setFields + ", set_modes & " + sm + " AS valid_set_modes FROM sets " +
+		whereConds + havingConds + " ORDER BY id DESC " + limit
+	rows, err := db.Query(setsQuery)
 
 	if err != nil {
 		return nil, err
@@ -120,12 +145,25 @@ func SearchSets(db, searchDB *sql.DB, opts SearchOptions) ([]Set, error) {
 		err = rows.Scan(
 			&s.ID, &s.RankedStatus, &s.ApprovedDate, &s.LastUpdate, &s.LastChecked,
 			&s.Artist, &s.Title, &s.Creator, &s.Source, &s.Tags, &s.HasVideo, &s.Genre,
-			&s.Language, &s.Favourites,
+			&s.Language, &s.Favourites, new(int),
 		)
 		if err != nil {
 			return nil, err
 		}
-		sets[setMap[s.ID]] = s
+		// we get the position we should place s in from the setMap, this way we
+		// keep the order of results as sphinx prefers.
+		pos, ok := setMap[s.ID]
+		if ok {
+			sets[pos] = s
+		} else {
+			sets = append(sets, s)
+			setIDs = append(setIDs, s.ID)
+			setMap[s.ID] = len(sets) - 1
+		}
+	}
+
+	if len(sets) == 0 {
+		return sets, nil
 	}
 
 	rows, err = db.Query(
