9bdfa825f3090d3005775df57a97e76caec4a454
Morgan Bazalgette <the@howl.moe>
2018-04-22 13:34:29 +0200

    Fix homepage showing 'vv2.1.0'

--- api/index.go
+++ api/index.go
@@ -5,11 +5,11 @@ import (
 )
 
 // Version is set by main and it is given to requests at /
-var Version = "2.x"
+var Version = "v2.DEV"
 
 func index(c *Context) {
 	c.WriteHeader("Content-Type", "text/plain; charset=utf-8")
-	c.Write([]byte("CheeseGull v" + Version + " Woo\nFor more information: https://github.com/osuripple/cheesegull"))
+	c.Write([]byte("CheeseGull " + Version + " Woo\nFor more information: https://github.com/osuripple/cheesegull"))
 }
 
 var _evh = expvar.Handler()
